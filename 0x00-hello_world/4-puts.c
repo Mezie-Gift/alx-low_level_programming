8d8741e1b692396b3f8265e14e2252d5081df9c5
Mezie-Gift <iammeziegift@gmail.com>
2022-09-08 09:35:29 -0700

    using puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-char Mystr[] = "programming is like building a multilingual puzzle\n";
+char Mystr[] = "\"programming is like building a multilingual puzzle\n";
 	puts(Mystr);
 	return (0); }
