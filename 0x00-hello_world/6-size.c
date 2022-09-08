64aad0f9553731502a510947a637b49daaa480f8
Mezie-Gift <iammeziegift@gmail.com>
2022-09-08 15:22:13 -0700

    using puts instead of printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(myInt));
+	printf("%lu\n", sizeof(myFloat));
+	printf("%lu\n", sizeof(myDouble));
+	printf("%lu\n", sizeof(myChar));
+	printf("%lu\n", sizeof(myLong int));
+	printf("%lu\n", sizeof(myLong long int));
+	return (0)
