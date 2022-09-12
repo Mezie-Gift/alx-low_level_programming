4f25c25505bfb092728713f0d8dc80bf5f958ebf
Mezie-Gift <iammeziegift@gmail.com>
2022-09-12 12:08:59 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program prints single digits starting from zero
+ * followed by a new line
+ * Return: return 0 (succes)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
