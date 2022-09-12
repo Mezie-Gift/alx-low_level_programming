841acf3ab575c7b47715d236eba6173e35acc7a0
Mezie-Gift <iammeziegift@gmail.com>
2022-09-12 16:19:14 -0700

    space and commar in c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program prints all possible combinations of a single-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '9')
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
