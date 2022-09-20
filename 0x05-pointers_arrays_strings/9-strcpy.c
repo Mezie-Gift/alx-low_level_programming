f6eb84832eef857e7a7d0481b5850cc7038bd1ec
Mezie-Gift <iammeziegift@gmail.com>
2022-09-20 08:59:23 -0700

    copy string pointed to to the buffer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,13 +3,14 @@
 /**
  * _strcpy - copies the string pointed to by src
  * @dest: a pointer to char
- * @src - a pointer to char that will be changed
+ * @src: a pointer to char that will be changed
  * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	i = 0;
 
 	while (src[i] != '\0')
