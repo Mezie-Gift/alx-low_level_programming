f0a1417e3e4343231d58a24167457f42bdcb5cbb
Mezie-Gift <iammeziegift@gmail.com>
2022-09-28 13:35:14 -0700

    print a string without using loop

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints string in reverse 
+ * _print_rev_recursion - prints string in reverse
  * @s: the string to be printed
  *
  *Return: nothing
