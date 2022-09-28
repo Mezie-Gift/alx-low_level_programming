e35ca6bbfedcbe65e27ac65751028daec92d7da6
Mezie-Gift <iammeziegift@gmail.com>
2022-09-28 13:25:40 -0700

    print string in reverse without loop

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,12 @@
  * _print_rev_recursion - prints string in reverse 
  * @s: the string to be printed
  *
- *Return: string
+ *Return: nothing
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
