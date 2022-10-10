e1710fe9bf92af9f4e4853c5aa5e6c198edf99cf
Mezie-Gift <iammeziegift@gmail.com>
2022-10-10 05:31:34 -0700

    struct to save a Dog's data

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dog - contains informations
+ * @name: the Dog's name
+ * @age: the Dog's age
+ * @owner: the Dog's owner
+ *
+ *Description: this struct contains informations regarding a Dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
