a20770be430ae4786954a4670ab8c00d2ee9426e
Mezie-Gift <iammeziegift@gmail.com>
2022-11-09 11:54:08 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,22 +2,31 @@
 
 /**
  * print_diagonal - prints a diagonal line based on integer n
- * @: the number of lines using '\' characters
+ * @n: the number of lines using '\' characters
  * Return: void
  */
 
-void print_diagonal(int)
+void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < n; n++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < n; j++)
 		{
-			_putchar(' ');
+			if (n <= 0)
+			{
+				_putchar(10);
+			}
+			if (j == i)
+			{
+				_putchar('\\');
+			}
+			else if (j < i)
+			{
+				_putchar(' ');
+			}
 		}
+		_putchar(10);
 	}
-	_putchar('\\');
-	_putchar('\n');
 }
